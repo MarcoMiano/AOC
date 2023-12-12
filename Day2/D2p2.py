ca14fafc6811db863b7cb1cf1038c498adfa345f
MarcoMiano <58668074+MarcoMiano@users.noreply.github.com>
2023-12-12 02:31:27 +0100

    [Day2]  Deleted useless comment

--- Day2/D2p2.py
+++ Day2/D2p2.py
@@ -1,19 +1,6 @@
 # AOC23 D2p1: Cube Cunundrum
 from D2p1 import parse_game_file
 
-# games = {
-#   1: {0: {'blue': 2, 'red': 3},
-#       1: {'blue': 3, 'green': 3, 'red': 6},
-#       2: {'blue': 4, 'red': 6},
-#       3: {'blue': 2, 'green': 2, 'red': 9},
-#       4: {'blue': 4, 'red': 2}},
-#   2: {0: {'green': 1, 'red': 4},
-#       1: {'red': 3},
-#       2: {'blue': 3, 'green': 13, 'red': 5},
-#       3: {'green': 3, 'red': 2},
-#       4: {'blue': 3, 'green': 3, 'red': 5},
-#       5: {'blue': 3, 'green': 12, 'red': 2}},
-# }
 COLORS = ["blue", "green", "red"]
 
 
