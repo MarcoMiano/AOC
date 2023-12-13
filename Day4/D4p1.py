203e4544e2b5cbf168bd4192a04aa86ffc6a2474
MarcoMiano <58668074+MarcoMiano@users.noreply.github.com>
2023-12-13 03:23:40 +0100

    [Day4]  Completed

---
+++ Day4/D4p1.py
@@ -0,0 +1,31 @@
+# AOC23 D4p1: Scratchcards
+
+
+def main() -> None:
+    with open("input.txt") as f:
+        input_file = f.read().strip().split("\n")
+
+    points = 0
+
+    for line in input_file:
+        count = 0
+        _, numbers = line.split(":")
+        winning_numbers, my_numbers = numbers.split("|")
+        winning_numbers = winning_numbers.strip().replace("  ", " ").split(" ")
+        my_numbers = my_numbers.strip().replace("  ", " ").split(" ")
+        for number in my_numbers:
+            if number in winning_numbers:
+                count += 1
+        match count:
+            case 0:
+                points += 0
+            case 1:
+                points += 1
+            case _:
+                points += 2 ** (count - 1)
+
+    print(points)
+
+
+if __name__ == "__main__":
+    main()
---
+++ Day4/D4p2.py
@@ -0,0 +1,72 @@
+# AOC23 D4p2: Scratchcards
+import re
+
+
+class Card(object):
+    def __init__(
+        self, card_number: int, winning_numbers: list, my_numbers: list
+    ) -> None:
+        self.number: int = card_number
+        self.winning_numbers: list[str] = winning_numbers
+        self.my_numbers: list[str] = my_numbers
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        pass
+
+
+class Cards(object):
+    def __init__(self) -> None:
+        self.cards: list = list()
+        self.count: int = 0
+
+    def append(self, new_object: Card, qty: int) -> None:
+        self.cards.append([new_object, qty])
+
+    def scratch(self) -> None:
+        for card, qty in self.cards:
+            win = 0
+            for number in card.my_numbers:
+                if number in card.winning_numbers:
+                    win += 1
+            for i in range(card.number, card.number + win):
+                self.cards[i][1] += qty
+        self.__private_count()
+
+    def __private_count(self) -> int:
+        for _, qty in self.cards:
+            self.count += qty
+        return self.count
+
+
+def parse_input_file(input_file) -> Cards:
+    cards = Cards()
+    for line in input_file:
+        card_number, numbers = line.split(":")
+
+        card_number = re.sub(r"\s+", "", card_number[4::])
+
+        winning_numbers, my_numbers = numbers.split("|")
+        winning_numbers = winning_numbers.strip().replace("  ", " ").split(" ")
+        my_numbers = my_numbers.strip().replace("  ", " ").split(" ")
+
+        card = Card(int(card_number), winning_numbers, my_numbers)
+        cards.append(card, 1)
+    return cards
+
+
+def main() -> None:
+    with open("input.txt") as f:
+        input_file = f.read().strip().split("\n")
+
+    cards = parse_input_file(input_file)
+
+    cards.scratch()
+
+    print(cards.count)
+
+
+if __name__ == "__main__":
+    main()
