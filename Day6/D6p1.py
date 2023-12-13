1894a8107671a5f188274cce4d67304fb30dc532
MarcoMiano <58668074+MarcoMiano@users.noreply.github.com>
2023-12-14 00:27:49 +0100

    [Day6]  Completed

---
+++ Day6/D6p1.py
@@ -0,0 +1,45 @@
+# AOC23 D6p1: Wait For It
+import re
+import math
+
+
+def parse_input_file(input_file: list[str]) -> list:
+    output = list()
+
+    input_file[0] = input_file[0].split(":")[1].strip()
+    input_file[0] = re.sub(r"\s+", ";", input_file[0]).split(";")
+
+    input_file[1] = input_file[1].split(":")[1].strip()
+    input_file[1] = re.sub(r"\s+", ";", input_file[1]).split(";")
+
+    if input_file[0].__len__() != input_file[1].__len__():
+        raise ValueError("Different numbers of elements in times and distances list")
+
+    for i in range(input_file[0].__len__()):
+        output.append([int(input_file[0][i]), int(input_file[1][i])])
+
+    return output
+
+
+def main() -> None:
+    with open("Day6\\input.txt") as f:
+        input_file = f.read().strip().split("\n")
+
+    races = parse_input_file(input_file)
+
+    wins = 1
+
+    for time, record_distance in races:
+        charge_of_record1 = (time - math.sqrt(time**2 - 4 * record_distance)) / 2
+        charge_of_record2 = (time + math.sqrt(time**2 - 4 * record_distance)) / 2
+        charge_of_record = min(charge_of_record1, charge_of_record2)
+
+        race_wins = (time - (int(charge_of_record) * 2)) - 1
+
+        wins *= race_wins
+
+    print(wins)
+
+
+if __name__ == "__main__":
+    main()
---
+++ Day6/D6p2.py
@@ -0,0 +1,34 @@
+# AOC23 D6p2: Wait For It
+import re
+import math
+
+
+def parse_input_file(input_file: list[str]) -> list:
+    input_file[0] = input_file[0].split(":")[1].strip()
+    input_file[0] = int(re.sub(r"\s+", "", input_file[0]))
+
+    input_file[1] = input_file[1].split(":")[1].strip()
+    input_file[1] = int(re.sub(r"\s+", "", input_file[1]))
+
+    print(input_file)
+
+    return input_file
+
+
+def main() -> None:
+    with open("Day6\\input.txt") as f:
+        input_file = f.read().strip().split("\n")
+
+    time, record_distance = parse_input_file(input_file)
+
+    charge_of_record1 = (time - math.sqrt(time**2 - 4 * record_distance)) / 2
+    charge_of_record2 = (time + math.sqrt(time**2 - 4 * record_distance)) / 2
+    charge_of_record = min(charge_of_record1, charge_of_record2)
+
+    answer = (time - (int(charge_of_record) * 2)) - 1
+
+    print(answer)
+
+
+if __name__ == "__main__":
+    main()
