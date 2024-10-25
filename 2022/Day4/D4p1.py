4ee564c4fed133ed1441069fbc83bc1e4a6598ac
MarcoMiano <58668074+MarcoMiano@users.noreply.github.com>
2024-10-25 20:35:08 +0200

    [22Day4] Part1: completed

---
+++ 2022/Day4/D4p1.py
@@ -0,0 +1,19 @@
+# AOC22 D4p1: Camp Cleanup
+
+
+def main() -> None:
+    overlapped = 0
+    with open("2022//Day4//input.txt") as f:
+        for line in f.readlines():
+            pair1, pair2 = [
+                tuple(map(int, pair.split("-"))) for pair in line.strip().split(",")
+            ]
+            if (pair1[0] <= pair2[0] and pair1[1] >= pair2[1]) or (
+                pair2[0] <= pair1[0] and pair2[1] >= pair1[1]
+            ):
+                overlapped += 1
+        print(overlapped)
+
+
+if __name__ == "__main__":
+    main()
