5296341283946be97c209992b21b6c5e83e80241
MarcoMiano <58668074+MarcoMiano@users.noreply.github.com>
2024-10-25 17:29:15 +0200

    [Day25] Added __main__ structure

--- 2023/Day25/D25p1.py
+++ 2023/Day25/D25p1.py
@@ -6,33 +6,39 @@ from pprint import pprint
 
 t_wiring: TypeAlias = dict[str, list[str]]
 
-nodes = set()
-edges = list()
-wiring: t_wiring = dict()
 
-with open("2023//Day25//input.txt") as f:
-    g_wiring = nx.Graph()
-    for line in f.readlines():
-        from_component = line.split(":")[0]
-        wiring[line.split(":")[0]] = line.split(": ")[1].split()
+def main() -> None:
+    nodes = set()
+    edges = list()
+    wiring: t_wiring = dict()
 
-    for l_comp, r_comps in wiring.items():
-        nodes.add(l_comp)
-        for comp in r_comps:
-            nodes.add(comp)
-            edges.append((l_comp, comp))
+    with open("2023//Day25//input.txt") as f:
+        g_wiring = nx.Graph()
+        for line in f.readlines():
+            from_component = line.split(":")[0]
+            wiring[line.split(":")[0]] = line.split(": ")[1].split()
 
-    g_wiring.add_nodes_from(nodes)
-    g_wiring.add_edges_from(edges)
+        for l_comp, r_comps in wiring.items():
+            nodes.add(l_comp)
+            for comp in r_comps:
+                nodes.add(comp)
+                edges.append((l_comp, comp))
 
-    communities = list(next(girvan_newman(g_wiring)))
+        g_wiring.add_nodes_from(nodes)
+        g_wiring.add_edges_from(edges)
 
-    ans = 1
+        communities = list(next(girvan_newman(g_wiring)))
 
-    for community in communities:
-        ans *= len(community)
+        ans = 1
 
-    print(ans)
+        for community in communities:
+            ans *= len(community)
 
-    # nx.draw(g_wiring, with_labels=True, node_color="skyblue")
-    # plt.show()
+        print(ans)
+
+        # nx.draw(g_wiring, with_labels=True, node_color="skyblue")
+        # plt.show()
+
+
+if __name__ == "__main__":
+    main()
