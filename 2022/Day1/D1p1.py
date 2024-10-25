8f2a755ff6f4d9c527297043027716d30668df63
MarcoMiano <58668074+MarcoMiano@users.noreply.github.com>
2024-10-25 18:01:35 +0200

    [22Day1] Part 1 completed

---
+++ 2022/Day1/D1p1.py
@@ -0,0 +1,18 @@
+def main() -> None:
+    elves: list[int] = list()
+    with open("2022//Day1//input.txt") as f:
+        elf_id = 0
+        for line in f.readlines():
+            if len(elves) == 0:
+                elves.append(0)
+            if line.strip() == "":
+                elf_id += 1
+                elves.append(0)
+                continue
+            elves[elf_id] += int(line.strip())
+
+    print(max(elves))
+
+
+if __name__ == "__main__":
+    main()
