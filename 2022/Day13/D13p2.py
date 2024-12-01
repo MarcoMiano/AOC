e35f24be3980a75e6e548328ddea8befee283efd
MarcoMiano <58668074+MarcoMiano@users.noreply.github.com>
2024-12-02 00:18:02 +0100

    [22Day13] Part 2 completed

--- 2022/Day13/D13p2.py
+++ 2022/Day13/D13p2.py
@@ -1,21 +1,8 @@
-# AOC22 D13p1: Distress Signal
+# AOC22 D13p2: Distress Signal
+
 import os
 
-from typing import TypeAlias
-from pprint import pprint
-
-t_packets: TypeAlias = list[tuple[list[int], list[int]]]
-
-
-def parse_input_file(input_path: str) -> t_packets:
-    packets: t_packets = list()
-    with open(input_path) as f:
-        raw_input: list[str] = f.read().split("\n\n")
-
-    for pair in raw_input:
-        left, right = pair.split()
-        packets.append((eval(left.strip()), eval(right.strip())))
-    return packets
+from functools import cmp_to_key
 
 
 def compare_pair(left_element: int | list[int], right_element: int | list[int]) -> int:
@@ -23,25 +10,25 @@ def compare_pair(left_element: int | list[int], right_element: int | list[int]) 
         if left_element == right_element:
             return 0
         elif left_element < right_element:
-            return 1
-        else:
             return -1
+        else:
+            return 1
     elif isinstance(left_element, list) and isinstance(right_element, list):
         for sub_left_element, sub_right_element in zip(left_element, right_element):
             match compare_pair(sub_left_element, sub_right_element):
-                case -1:
-                    return -1
-                case 0:
-                    continue
                 case 1:
                     return 1
+                case 0:
+                    continue
+                case -1:
+                    return -1
         else:
             if len(left_element) == len(right_element):
                 return 0
             elif len(left_element) < len(right_element):
-                return 1
-            else:
                 return -1
+            else:
+                return 1
     elif isinstance(left_element, list) and isinstance(right_element, int):
         return compare_pair(left_element, [right_element])
     elif isinstance(left_element, int) and isinstance(right_element, list):
@@ -50,20 +37,27 @@ def compare_pair(left_element: int | list[int], right_element: int | list[int]) 
         assert False, f"Unreacheable {type(left_element), type(right_element)}"
 
 
+def parse_input_file(input_path: str) -> list[int | list]:
+    packets: list[int | list] = list()
+    with open(input_path) as f:
+        raw_input: list[str] = f.read().split("\n\n")
+    raw_input = [item for pair in raw_input for item in pair.split()]
+
+    for packet in raw_input:
+        packets.append(eval(packet))
+    return packets
+
+
 def main() -> None:
     input_path: str = os.path.dirname(__file__) + "\\input.txt"
-    packets: t_packets = parse_input_file(input_path)
+    packets: list[int | list] = parse_input_file(input_path)
 
-    ok_pairs = 0
-    for i, (left, right) in enumerate(packets):
-        result = compare_pair(left, right)
-        if result > 0:
-            ok_pairs += i + 1
-            continue
-        else:
-            continue
+    packets.append([[2]])
+    packets.append([[6]])
 
-    print(ok_pairs)
+    packets = sorted(packets, key=cmp_to_key(compare_pair))
+
+    print((packets.index([[2]]) + 1) * (packets.index([[6]]) + 1))
 
 
 if __name__ == "__main__":
